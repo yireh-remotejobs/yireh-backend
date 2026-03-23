04f70237cc0a9b6a665d7c33aa466e63b1371d49
Yireh Ai <yireh.remotejobs@gmail.com>
2026-03-23 14:02:20 +0530

    Update jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,22 +1,18 @@
-const protect = require("../middleware/authMiddleware");
 const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
+const protect = require("../middleware/protect");
 
-// GET all jobs
+// GET ALL
 router.get("/", async (req, res) => {
   const jobs = await Job.find().sort({ createdAt: -1 });
   res.json(jobs);
 });
 
-// POST new job
+// POST
 router.post("/", protect, async (req, res) => {
   const job = await Job.create(req.body);
   res.json(job);
 });
 
 module.exports = router;
-router.get("/download-cv/:id", protect, async (req, res) => {
-  const app = await InternshipApplication.findById(req.params.id);
-  res.download(app.cv);
-});
