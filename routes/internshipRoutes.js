bff4edf0ac3f2d674b655e2382ea8435ad3dd3e5
Yireh Ai <yireh.remotejobs@gmail.com>
2026-03-23 14:03:24 +0530

    Update internshipRoutes.js

--- routes/internshipRoutes.js
+++ routes/internshipRoutes.js
@@ -1,44 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-
 const Internship = require("../models/Internship");
-const InternshipApplication = require("../models/InternshipApplication");
-const { sendToCompany } = require("../services/emailService");
+const protect = require("../middleware/protect");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+router.get("/", async (req, res) => {
+  const data = await Internship.find().sort({ createdAt: -1 });
+  res.json(data);
 });
 
-const upload = multer({ storage });
-
-router.post("/:id", upload.single("cv"), async (req, res) => {
-  try {
-    const { fullName, email } = req.body;
-
-    const internship = await Internship.findById(req.params.id);
-
-    const application = await InternshipApplication.create({
-      internship: internship._id,
-      fullName,
-      email,
-      cv: req.file.path
-    });
-
-    await sendToCompany(
-      internship.email,
-      { fullName, email },
-      req.file.path
-    );
-
-    res.json({ message: "Application submitted successfully" });
-
-  } catch (err) {
-    res.status(500).json({ message: "Error applying" });
-  }
+router.post("/", protect, async (req, res) => {
+  const item = await Internship.create(req.body);
+  res.json(item);
 });
 
 module.exports = router;
