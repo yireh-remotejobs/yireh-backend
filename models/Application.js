bc18237a94882e8cd0f1929a40fa48443716f32b
Yireh Ai <yireh.remotejobs@gmail.com>
2026-03-23 13:55:40 +0530

    Update Application.js

--- models/Application.js
+++ models/Application.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
+const applicationSchema = new mongoose.Schema({
+  job: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
+  internship: { type: mongoose.Schema.Types.ObjectId, ref: "Internship" },
+
+  fullName: String,
+  email: String,
+  cv: String,
+
+  status: {
+    type: String,
+    default: "Pending"
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Application", applicationSchema);const mongoose = require("mongoose");
+
 const applicationSchema = new mongoose.Schema(
   {
     job: {
