1cc89b75715b303b169bdfee2d1781e5d6d2193f
Yireh Ai <yireh.remotejobs@gmail.com>
2026-03-31 00:55:16 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,23 +1,19 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, text, file) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
+const sendEmail = async (to, subject, text) => {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
     subject,
-    text,
-    attachments: file ? [{
-      filename: file.originalname,
-      content: file.buffer
-    }] : []
+    text
   });
 };
 
